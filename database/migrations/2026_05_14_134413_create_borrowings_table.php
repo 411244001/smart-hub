bec4085e68fffc3dcc472b9f2c762abf318bfbc4
Waluyo <411244001@mahasiswa.undira.ac.id>
2026-05-15 09:32:09 +0700

    membuat data dummy di users dan equipment

--- database/migrations/2026_05_14_134413_create_borrowings_table.php
+++ database/migrations/2026_05_14_134413_create_borrowings_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
         Schema::create('borrowings', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->foreignId('equipment_id')->constrained()->onDelete('cascade');
+            $table->foreignId('equipment_id')->constrained('equipments')->onDelete('cascade');
             $table->date('borrow_date');
             $table->date('return_date')->nullable();
             $table->enum('status', ['pending', 'approved', 'returned', 'rejected'])
